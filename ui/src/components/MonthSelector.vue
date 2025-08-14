210fc523995ca4ddf6c59148baec0b2d2356fbaf
Eric Chiesse <echiesse@gmail.com>
2025-08-13 23:55:47 -0300

    Criados componentes para selecionar mês e ano.

---
+++ ui/src/components/MonthSelector.vue
@@ -0,0 +1,25 @@
+<template>
+    <select v-model="model" @change="onMonthSelected">
+        <option v-for="monthNumber in months" :value="monthNumber">{{ monthNumber }}</option>
+    </select>
+</template>
+
+
+<script setup>
+
+import { ref, defineModel } from 'vue';
+
+const emit = defineEmits(['onMonthSelected'])
+
+const currentTime = new Date()
+const currentMonth = currentTime.getMonth() + 1
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const model = defineModel()
+model.value = model.value || currentMonth
+
+function onMonthSelected(e) {
+    emit('onMonthSelected', model.value)
+}
+
+</script>
---
+++ ui/src/components/YearSelector.vue
@@ -0,0 +1,26 @@
+<template>
+    <select v-model="model" @change="onYearSelected">
+        <option v-for="year in years" :value="year">{{ year }}</option>
+    </select>
+</template>
+
+
+<script setup>
+
+import {ref, defineModel} from 'vue'
+
+const emit = defineEmits(['yearSelected'])
+
+const currentTime = new Date()
+const currentYear = currentTime.getFullYear()
+const years = [0, 1].map(delta => currentYear + delta)
+
+const model = defineModel()
+model.value = model.value || currentYear
+
+function onYearSelected(event) {
+    emit('yearSelected', model.value)
+}
+
+
+</script>
